d9bab56a98b28acff8a124177fc149ca2e88d527
Larry Li <larry.zy.li@gmail.com>
2018-03-16 11:38:21 -0500

    allow palindrome all cases

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,55 +1,51 @@
 import React, { Component } from "react";
 
 class Palindrome extends Component {
-    constructor() {
-        super();
-        this.state = {
-            palindrome: "",
-            userInput: ""
-        };
-        this.updateInput = this.updateInput.bind(this);
-        this.checkPalindrome = this.checkPalindrome.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      palindrome: "",
+      userInput: ""
+    };
+    this.updateInput = this.updateInput.bind(this);
+    this.checkPalindrome = this.checkPalindrome.bind(this);
+  }
+  updateInput(val) {
+    this.setState({
+      userInput: val
+    });
+  }
+  checkPalindrome() {
+    let backwards = this.state.userInput
+      .toLowerCase()
+      .split("")
+      .reverse()
+      .join("");
+    if (this.state.userInput.toLowerCase() === backwards) {
+      this.setState({
+        palindrome: "true"
+      });
+    } else {
+      this.setState({
+        palindrome: "false"
+      });
     }
-    updateInput(val) {
-        this.setState({
-            userInput: val
-        });
-    }
-    checkPalindrome() {
-        let backwards = this.state.userInput
-            .split("")
-            .reverse()
-            .join("");
-        if (this.state.userInput === backwards) {
-            this.setState({
-                palindrome: "true"
-            });
-        } else {
-            this.setState({
-                palindrome: "false"
-            });
-        }
-    }
-    render() {
-        return (
-            <div className="puzzleBox palindromePB">
-                <h4>Palindrome</h4>
-                <input
-                    onChange={e => this.updateInput(e.target.value)}
-                    className="inputLine"
-                />
-                <button
-                    onClick={this.checkPalindrome}
-                    className="confirmationButton"
-                >
-                    Check
-                </button>
-                <span className="resultsBox">
-                    Palindrome: {this.state.palindrome}
-                </span>
-            </div>
-        );
-    }
+  }
+  render() {
+    return (
+      <div className="puzzleBox palindromePB">
+        <h4>Palindrome</h4>
+        <input
+          onChange={e => this.updateInput(e.target.value)}
+          className="inputLine"
+        />
+        <button onClick={this.checkPalindrome} className="confirmationButton">
+          Check
+        </button>
+        <span className="resultsBox">Palindrome: {this.state.palindrome}</span>
+      </div>
+    );
+  }
 }
 
 export default Palindrome;
